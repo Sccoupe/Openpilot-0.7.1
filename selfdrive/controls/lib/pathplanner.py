1df733f36d621dc58f40a6ef15fd2955f0f0a77c
Sccoupe <46354555+Sccoupe@users.noreply.github.com>
2020-01-27 11:45:12 -0500

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -1,5 +1,7 @@
 import os
 import math
+import numpy as np
+from common.numpy_fast import clip
 from common.realtime import sec_since_boot, DT_MDL
 from selfdrive.swaglog import cloudlog
 from selfdrive.controls.lib.lateral_mpc import libmpc_py
@@ -8,6 +10,8 @@ from selfdrive.controls.lib.lane_planner import LanePlanner
 from selfdrive.config import Conversions as CV
 import cereal.messaging as messaging
 from cereal import log
+import os.path
+import json
 
 LaneChangeState = log.PathPlan.LaneChangeState
 LaneChangeDirection = log.PathPlan.LaneChangeDirection
@@ -53,7 +57,12 @@ class PathPlanner():
 
     self.setup_mpc()
     self.solution_invalid_cnt = 0
-    self.path_offset_i = 0.0
+    self.frame = 0
+    if os.path.exists('/data/curvature.json'):
+        with open("/data/curvature.json", "r") as f:
+            self.curvature_offset_i = json.load(f)	
+    else:
+        self.curvature_offset_i = 0.0
     self.lane_change_state = LaneChangeState.off
     self.lane_change_timer = 0.0
     self.prev_one_blinker = False
@@ -84,7 +93,7 @@ class PathPlanner():
     # Run MPC
     self.angle_steers_des_prev = self.angle_steers_des_mpc
     VM.update_params(sm['liveParameters'].stiffnessFactor, sm['liveParameters'].steerRatio)
-    curvature_factor = VM.curvature_factor(v_ego)
+    curvature_factor = VM.curvature_factor(v_ego) + self.curvature_offset_i
 
     self.LP.parse_model(sm['model'])
 
@@ -151,12 +160,19 @@ class PathPlanner():
 
 
     # TODO: Check for active, override, and saturation
-    # if active:
-    #   self.path_offset_i += self.LP.d_poly[3] / (60.0 * 20.0)
-    #   self.path_offset_i = clip(self.path_offset_i, -0.5,  0.5)
-    #   self.LP.d_poly[3] += self.path_offset_i
-    # else:
-    #   self.path_offset_i = 0.0
+    if active and angle_steers - angle_offset > 0.5:
+      self.curvature_offset_i -= self.LP.d_poly[3] / 12000
+      #self.LP.d_poly[3] += self.curvature_offset_i
+    elif active and angle_steers - angle_offset < -0.5:
+      self.curvature_offset_i += self.LP.d_poly[3] / 12000
+
+    self.curvature_offset_i = clip(self.curvature_offset_i, -0.3, 0.3)
+    self.frame += 1
+    if self.frame == 12000: #every 2 mins
+      with open("/data/curvature.json", "w") as f:
+          json.dump(self.curvature_offset_i, f)
+      os.chmod("/data/curvature.json", 0o777)
+      self.frame = 0
 
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
