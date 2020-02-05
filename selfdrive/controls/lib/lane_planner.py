4701e5fe0c57b87f636786618f58411db4e432e8
Sccoupe <46354555+Sccoupe@users.noreply.github.com>
2020-02-05 17:42:22 -0500

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -1,9 +1,8 @@
 from common.numpy_fast import interp
 import numpy as np
 from cereal import log
-from selfdrive.car.toyota.carstate import CAN_CAMERA_OFFSET
 
-CAMERA_OFFSET = CAN_CAMERA_OFFSET   #0.06  # m from center car to camera Increase if too far left 0.03 at a time.
+CAMERA_OFFSET = car.carstate.CAN_CAMERA_OFFSET   #0.06  # m from center car to camera Increase if too far left 0.03 at a time.
 
 def compute_path_pinv(l=50):
   deg = 3
