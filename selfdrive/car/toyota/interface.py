90df6fa7d2b6ac32e28239fd9c79db28bae0b200
Sccoupe <46354555+Sccoupe@users.noreply.github.com>
2020-02-16 13:03:21 -0500

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -110,8 +110,8 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 18.27
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]] #Lower kp to lessen oscilation
-      ret.lateralTuning.pid.kf = 0.00003   # full torque for 20 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]     #[[0.2], [0.05]] #Lower kp to lessen oscilation
+      ret.lateralTuning.pid.kf = 0.00007818594     #0.00003   # full torque for 20 deg at 80mph means 0.00007818594
 
     elif candidate == CAR.LEXUS_RXH:
       stop_and_go = True
